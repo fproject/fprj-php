7d7a16015d41563ef300eaab3c7ef3c12f555dc6
Bui Sy Nguyen <nguyenbs@projectkit.net>
2016-01-19 10:03:51 +0700

    First commit

---
+++ tests/WorkCalendarTest.php
@@ -0,0 +1,11 @@
+<?php
+class WorkCalendarTest extends PHPUnit_Framework_TestCase
+{
+    private $params = [];
+
+    public function testWorkCalendar()
+    {        
+        $this->assertTrue(true);
+    }
+}
+?>
