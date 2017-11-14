d9b9adadf89d5617dd5277c7fc9a16cd1b1f5e52
Bui Sy Nguyen <nguyenbs@projectkit.net>
2017-11-14 08:35:46 +0700

    Update MyBusiness.php

--- mypkg/MyBusiness.php
+++ mypkg/MyBusiness.php
@@ -5,5 +5,11 @@ class Period
 {
     public function sayHello($a, $b)
     {
+        $s = "Hello";
+        if($a && $b)
+        {
+            $s = $s . " world";
+        }
+        return $s;
     }
 }
