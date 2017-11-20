31a1d175dec4433067bed6849d86611540d1a338
Bui Sy Nguyen <nguyenbs@projectkit.net>
2017-11-20 18:29:57 +0700

    Update WorkCalendarTest.php

--- tests/WorkCalendarTest.php
+++ tests/WorkCalendarTest.php
@@ -10,5 +10,12 @@ class WorkCalendarTest extends PHPUnit_Framework_TestCase
         $workCal->name = "My Calendar";
         $this->assertTrue($workCal->name === "My Calendar");
     }
+    
+    public function testWorkCalender2()
+    {
+         $workCal = new WorkCalendar();
+        $workCal->id = "Some Value";
+        $this->assertTrue($workCal->id === "Some Value 1");
+    }
 }
 ?>
