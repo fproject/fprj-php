00fde61d5b09cc21d81a42b2f3bd99a6b416f0d8
Bui Sy Nguyen <nguyenbs@projectkit.net>
2017-11-21 08:12:45 +0700

    Update WorkCalendarTest.php

--- tests/WorkCalendarTest.php
+++ tests/WorkCalendarTest.php
@@ -1,5 +1,6 @@
 <?php
 use fproject\calendar\WorkCalendar;
+use fproject\calendar\Period;
 class WorkCalendarTest extends PHPUnit_Framework_TestCase
 {
     private $params = [];
@@ -16,6 +17,13 @@ class WorkCalendarTest extends PHPUnit_Framework_TestCase
          $workCal = new WorkCalendar();
         $workCal->id = "Some Value";
         $this->assertTrue($workCal->id === "Some Value");
+    }
+    
+    public function testWorkCalender3()
+    {
+        $workCal = new WorkCalendar();
+        $workCal->periods = [new Period()];
+        $this->assertTrue(count($workCal->periods) === 1);
     }
 }
 ?>
